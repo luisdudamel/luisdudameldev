6d1787ef9e4486d8eeb28b5b7d484a1f04272df9
Luis Dudamel <ladusa@gmail.com>
2022-08-06 19:32:32 +0200

    Added favicon and main title

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import MainStyled from "./MainStyled";
+
+const Main = (): JSX.Element => {
+  return (
+    <MainStyled>
+      <h1>IN DEVELOPMENT..</h1>
+    </MainStyled>
+  );
+};
+
+export default Main;
---
+++ src/components/Main/MainStyled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const MainStyled = styled.div`
+  h1 {
+    color: white;
+  }
+`;
+
+export default MainStyled;
