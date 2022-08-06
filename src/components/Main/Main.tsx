876a4bf2d44cb96238bf1bba75f8942b28c3a76b
Luis Dudamel <ladusa@gmail.com>
2022-08-06 20:26:50 +0200

    Added social icons and links for them

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -9,6 +9,24 @@ const Main = (): JSX.Element => {
         <span className="loading-dot">.</span>
         <span className="loading-dot">.</span>
       </h1>
+      <div className="social-icons">
+        <a href="https://github.com/luisdudamel">
+          <img
+            className="social-icons--icon"
+            width={30}
+            src="img/github.png"
+            alt="Github Logo"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/luis-dudamel/">
+          <img
+            className="social-icons--icon"
+            width={30}
+            src="img/linkedin.png"
+            alt="Linkedin Logo"
+          />
+        </a>
+      </div>
     </MainStyled>
   );
 };
