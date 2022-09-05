badc5436c802bb8584668974b1530da407054ed3
Luis Dudamel <ladusa@gmail.com>
2022-09-05 23:21:07 +0200

    Added scroll to top button and functionality

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,41 @@
+import { useState } from "react";
 import About from "../About/About";
+
 import Contact from "../Contact/Contact";
 import Hero from "../Hero/Hero";
 import Projects from "../Projects/Projects";
-import MainStyled from "./MainStyled";
+import { GoToTopStyled, MainStyled } from "./MainStyled";
 
 const Main = (): JSX.Element => {
+  const [scrollVisible, setScrollVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setScrollVisible(true);
+    } else if (scrolled <= 300) {
+      setScrollVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  window.addEventListener("scroll", toggleVisible);
   return (
     <MainStyled>
+      <GoToTopStyled onClick={scrollToTop}>
+        <img
+          className={scrollVisible ? "scrolled" : ""}
+          src="./img/arrowup.png"
+          width={20}
+          alt="Go to top arrow"
+        />
+      </GoToTopStyled>
       <Hero />
       <About />
       <Projects />
--- src/components/Main/MainStyled.tsx
+++ src/components/Main/MainStyled.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const MainStyled = styled.div`
+export const MainStyled = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -8,4 +8,20 @@ const MainStyled = styled.div`
   min-height: 100vh;
 `;
 
-export default MainStyled;
+export const GoToTopStyled = styled.div`
+  position: fixed;
+  bottom: 10px;
+  right: 4vw;
+  z-index: 20;
+  width: 25px;
+  transition: all 0.4s;
+  img {
+    opacity: 0;
+    transition: all 0.4s;
+  }
+
+  img.scrolled {
+    opacity: 100;
+    transition: all 0.4s;
+  }
+`;
