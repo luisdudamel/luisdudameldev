c989db82094e4e6f3d83d26038f4ed114fdbe927
Luis Dudamel <ladusa@gmail.com>
2022-09-05 21:12:38 +0200

    Added AOS to contact section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -9,16 +9,32 @@ import {
 const Contact = (): JSX.Element => {
   return (
     <ContactStyled>
-      <ContactTitle>Let's talk!</ContactTitle>
-      <ContactArticle>
+      <ContactTitle data-aos="fade-right" data-aos-delay="300">
+        Let's talk!
+      </ContactTitle>
+      <ContactArticle data-aos="fade-right" data-aos-offset="300">
         I’m always looking for new oportunities. Whether you want to say hi or
         have a question. My inbox is always open.{" "}
       </ContactArticle>
       <ContactIconsContainer>
-        <ContactButton>Get in touch</ContactButton>
-        <img src="./img/techicons/linkedin-logo.png" width={50} alt="" />
-        <img src="./img/techicons/twitter-logo.png" width={50} alt="" />
-        <p>
+        <ContactButton data-aos="zoom-in" data-aos-offset="300">
+          Get in touch
+        </ContactButton>
+        <img
+          data-aos="zoom-in"
+          data-aos-offset="300"
+          src="./img/techicons/linkedin-logo.png"
+          width={50}
+          alt=""
+        />
+        <img
+          data-aos="zoom-in"
+          data-aos-offset="300"
+          src="./img/techicons/twitter-logo.png"
+          width={50}
+          alt=""
+        />
+        <p data-aos="fade-right" data-aos-offset="200">
           <span>Built</span> and <span>designed</span> by Luis Dudamel
         </p>
       </ContactIconsContainer>
