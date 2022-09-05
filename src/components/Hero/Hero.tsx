1129477662a92817db269cddd0240bc917d4a227
Luis Dudamel <ladusa@gmail.com>
2022-09-05 21:08:21 +0200

    Added fadeout to projects section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -23,14 +23,14 @@ const Hero = (): JSX.Element => {
         <Element name="home">
           <Hamburger />
         </Element>
-        <HeroSubhead data-aos="fade-down">Hi, I'm</HeroSubhead>
-        <HeroName data-aos="slide-right">
+        <HeroSubhead data-aos="slide-down">Hi, I'm</HeroSubhead>
+        <HeroName data-aos="slide-right" data-aos-delay="200">
           Luis Dudamel<HeroDot>.</HeroDot>
         </HeroName>
-        <HeroTitle data-aos="slide-up" data-aos-offset="100">
+        <HeroTitle data-aos="fade-up" data-aos-delay="400">
           I create stuff <br /> for the web
         </HeroTitle>
-        <HeroText data-aos="zoom-in" data-aos-offset="200">
+        <HeroText data-aos="fade-up" data-aos-delay="500">
           I'm a software engineer, who loves to find new ways of solving
           problems while building great digital experiences.
         </HeroText>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import {
   MoreProjectsContainer,
   ProjectDescriptionContainer,
@@ -7,19 +8,32 @@ import {
   ProjectsStyled,
   ProjectsTitle,
 } from "./ProjectsStyled";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Projects = (): JSX.Element => {
+  useEffect(() => {
+    Aos.init({ duration: 500, easing: "ease-in-out-cubic", once: true });
+  }, []);
+
   return (
     <ProjectsStyled>
-      <ProjectsTitle>Curently working on</ProjectsTitle>
+      <ProjectsTitle data-aos="fade-right" data-aos-delay="200">
+        Curently working on
+      </ProjectsTitle>
       <ProjectItem>
         <ProjectItemContainer>
           <img
             src="./img/projects/barkedin-mobile.png"
             height={300}
             alt="Mobile phone showing barkedin app"
+            data-aos="fade-right"
+            data-aos-offset="400"
           />
-          <ProjectDescriptionContainer>
+          <ProjectDescriptionContainer
+            data-aos="fade-right"
+            data-aos-offset="300"
+          >
             <h4>BarkedIn</h4>
             <ProjectItemDescription>
               Social network for dogs. <br /> CRUD SPA built with React +
@@ -37,22 +51,28 @@ const Projects = (): JSX.Element => {
                 src="./img/techicons/github-orange.png"
                 height={40}
                 alt="Github logo"
+                data-aos="zoom-in"
+                data-aos-offset="350"
               />
               <img
                 src="./img/techicons/web-orange.png"
                 height={40}
                 alt="Website logo"
+                data-aos="zoom-in"
+                data-aos-offset="400"
               />
             </div>
           </ProjectDescriptionContainer>
         </ProjectItemContainer>
       </ProjectItem>
-      <MoreProjectsContainer>
+      <MoreProjectsContainer data-aos="fade-right" data-aos-delay="300">
         Looking for more? <br /> Check out my GitHub!
         <img
           src="./img/techicons/github-orange.png"
           height={60}
           alt="Github logo"
+          data-aos="fade-right"
+          data-aos-offset="300"
         />
       </MoreProjectsContainer>
     </ProjectsStyled>
